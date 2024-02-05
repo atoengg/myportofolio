1c494bc8225c4d39b9c6010d260e2f323f77ada1
karimDev <kromosom06@gmail.com>
2024-02-05 10:47:55 +0700

    add: float btn use react scroll-to-top

--- src/layouts/Layouts.jsx
+++ src/layouts/Layouts.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Navbar from "../components/Fragments/Navbar/Navbar";
+import ScrollToTop from "react-scroll-to-top";
+import { BsRocketFill } from "react-icons/bs";
 
 const Layouts = ({ children }) => {
   return (
@@ -7,6 +9,18 @@ const Layouts = ({ children }) => {
       <Navbar />
       <main className="">
         <div className="container mx-auto">{children}</div>
+        <ScrollToTop
+          top={200}
+          smooth
+          component={<BsRocketFill size={25} color="#d1d5db" />}
+          style={{
+            backgroundColor: "#111827",
+            borderRadius: "100%",
+            width: "60px",
+            height: "60px",
+          }}
+          className="flex items-center justify-center top-[80%] lg:top-[85%]"
+        />
       </main>
     </>
   );
