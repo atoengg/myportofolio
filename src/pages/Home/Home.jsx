9921022c2ca274e6c99a2bf2fa7091fde4e41aec
karimDev <kromosom06@gmail.com>
2024-02-08 16:18:54 +0700

    add: aos animation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import ButtonLink from "../../components/Elements/Button/ButtonLink/ButtonLink";
 import resume from "../../assets/pdf/CV_Mohammad Irham alkarim.pdf";
 import { BsFileEarmarkArrowDownFill } from "react-icons/bs";
 import { useTranslation } from "react-i18next";
+import AOS from "aos";
 
 const Home = () => {
   const { t } = useTranslation();
@@ -21,23 +22,43 @@ const Home = () => {
     window.scrollTo(0, 0);
   }, []);
 
+  useEffect(() => {
+    AOS.init({
+      once: true,
+    });
+  });
+
   return (
     <div>
       <Layouts>
         <section className="lg:min-h-screen py-20">
           <div className="hero container ">
             <div className="hero-content flex flex-col lg:flex-row-reverse">
-              <div className=" ">
+              <div
+                className=""
+                data-aos="fade-left"
+                data-aos-duration="3000"
+                data-aos-delay="1200"
+              >
                 <img
                   src={imgHero}
                   className="max-w-full mx-auto md:w-60 lg:w-80"
                 />
               </div>
               <div className="w-full self-center p-4">
-                <h1 className="text-3xl font-bold md:text-5xl ">
+                <h1
+                  className="text-3xl font-bold md:text-5xl"
+                  data-aos="fade-right"
+                  data-aos-duration="2000"
+                >
                   {t("greeting")} {typeAnimation} <Cursor cursorStyle="|" />
                 </h1>
-                <p className="py-6 text-base leading-normal text-justify lg:text-left">
+                <p
+                  className="py-6 text-base leading-normal text-justify lg:text-left"
+                  data-aos="fade-in"
+                  data-aos-duration="2000"
+                  data-aos-delay="1000"
+                >
                   {t("introduction")}
                 </p>
                 <ButtonLink
